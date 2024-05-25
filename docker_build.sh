ec8047a8ae1699b3b9cb2e1e1b2c9ed53cb10e77
Pedro Rauiz Estigarribia Gestal <pedro@beamable.com>
2024-05-25 20:36:02 -0300

    Test building maps

--- docker_build.sh
+++ docker_build.sh
@@ -4,9 +4,9 @@ docker pull ghcr.io/epicgames/unreal-engine:dev-5.3.2
 
 argument=${1:-"nothing"}
 if [ $argument = "server" ]; then
-    docker build --build-arg BUILD_TYPE=-server --build-arg MAP_ARG=-map=DefaultMap+BeamBackendTests --progress plain -t unrealrunner .
+    docker build --build-arg BUILD_TYPE=-server --build-arg --progress plain -t unrealrunner .
 else
-    docker build --build-arg MAP_ARG=-map=DefaultMap+BeamBackendTests --progress plain -t unrealrunner .
+    docker build --build-arg --progress plain -t unrealrunner .
 fi
 
 docker run --name unreal_container -d unrealrunner:latest
