4a9071e5308d0008b1d731f6ab4248aa37fa9abe
Piotr Siuszko <siuszko@zoho.com>
2024-06-05 13:16:10 +0200

    String comparision fix

--- docker_build.sh
+++ docker_build.sh
@@ -2,18 +2,21 @@
 
 docker pull ghcr.io/epicgames/unreal-engine:dev-5.3.2
 
-argument=${1:-"nothing"}
+argument=${1:-"client"}
+argument_two=${2:-""}
+
 if [ $argument = "server" ]; then
+    echo "Building server"
     docker build --build-arg BUILD_TYPE=-server --progress plain -t unrealrunner .
 else
+    echo "Building client"
     docker build --progress plain -t unrealrunner .
 fi
 
 docker run --name unreal_container -d unrealrunner:latest
 docker cp unreal_container:/home/ue4/PackagedProject .
 
-argument_two=${2:-"nothing"}
-if [ $argument_two = "copy_build" ]; then
+if [ $argument_two = "cache" ]; then
     echo "Copying Intermediate and Saved directories from container"
     docker cp unreal_container:/home/ue4/project/Intermediate .
     docker cp unreal_container:/home/ue4/project/Saved .
