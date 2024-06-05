83a69af7295fc560ed82f3736db1009f32241a2f
Piotr Siuszko <siuszko@zoho.com>
2024-06-05 14:30:44 +0200

    Image name improvements

--- docker_build.sh
+++ docker_build.sh
@@ -2,18 +2,29 @@
 
 docker pull ghcr.io/epicgames/unreal-engine:dev-5.3.2
 
+if [ -s "BeamProjOverride.txt" ]; then
+    project_id=$(<"BeamProjOverride.txt")
+else
+    project_id="BEAMPROJ_Sandbox"
+fi
+
 argument=${1:-"client"}
 argument_two=${2:-""}
+image_name="$argument"
+image_name+="_"
+image_name+="$project_id"
+image_name="${image_name,,}"
+echo "Resulting image: $image_name"
 
 if [ $argument = "server" ]; then
     echo "Building server"
-    docker build --build-arg BUILD_TYPE=-server --progress plain -t unrealrunner .
+    docker build --build-arg BUILD_TYPE=-server --progress plain -t $image_name .
 else
     echo "Building client"
-    docker build --progress plain -t unrealrunner .
+    docker build --progress plain -t $image_name .
 fi
 
-docker run --name unreal_container -d unrealrunner:latest
+docker run --name unreal_container -d $image_name:latest
 docker cp unreal_container:/home/ue4/PackagedProject .
 
 if [ $argument_two = "cache" ]; then
