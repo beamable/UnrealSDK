7b2d3159f1bff9e38828bf20db8d6c1fa11760e4
Piotr Siuszko <siuszko@zoho.com>
2024-06-07 14:04:53 +0200

    Windows build rework

--- windows_build_project.ps1
+++ windows_build_project.ps1
@@ -1,16 +1,39 @@
 $buildType = $args[0]
 
+# Check if BeamProjOverride.txt exists and is not empty
+if (Test-Path "BeamProjOverride.txt") {
+    $content = Get-Content "BeamProjOverride.txt" | Out-String
+    if ($content.Trim()) {
+        $project_id = $content.Trim()
+    } else {
+        $project_id = "BEAMPROJ_Sandbox"
+    }
+} else {
+    $project_id = "BEAMPROJ_Sandbox"
+}
+
+# Get the first argument, default to "client" if not provided
+$argument = if ($args.Count -ge 1) { $args[0] } else { "client" }
+
+# Create the image name
+$image_name = "${argument}_${project_id}_win64"
+
+# Convert to lowercase
+$image_name = $image_name.ToLower()
+
+Write-Output "Resulting image: $image_name"
 docker image prune
 
 if ($buildType -eq 'server') {
-    docker build --build-arg BUILD_TYPE=-server -t unreal-win64-project -f WinDockerfile .
+    docker build --build-arg BUILD_TYPE=-server -t $image_name -f WinDockerfile .
 } else {
-    docker build -t unreal-win64-project -f WinDockerfile .
+    docker build -t $image_name -f WinDockerfile .
 }
-docker run --name unreal-project -d unreal-win64-project
-docker cp unreal-project:C:\Project\PackagedProject .
-docker cp unreal_container:C:\Project\Intermediate .
-docker cp unreal_container:C:\Project\Saved .
+$parentDirectory = Split-Path $MyInvocation.MyCommand.Path -Parent
+
+& docker run --name unreal-project -v "${parentDirectory}:C:\Project" "${image_name}:latest"
+
+docker cp unreal-project:C:\PackagedProject .
 
 docker stop unreal-project
 docker rm unreal-project
