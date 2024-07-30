e13eb6a3ed71c046603243651d49ebe2c87493e2
Piotr Siuszko <siuszko@zoho.com>
2024-07-30 15:48:17 +0200

    Windows build script is in bash now

---
+++ windows_build.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+buildType=${1:-"client"}
+platform=${2:-"Win64"}
+extraArg=""
+if [ "$buildType" == "server" ]; then
+    extraArg="-server"
+fi
+
+# Check if BeamProjOverride.txt exists and is not empty
+if [ -f "BeamProjOverride.txt" ]; then
+    content=$(<BeamProjOverride.txt)
+    if [ -n "$content" ]; then
+        projectId=$(echo "$content" | xargs)
+    else
+        projectId="BEAMPROJ_Sandbox"
+    fi
+else
+    projectId="BEAMPROJ_Sandbox"
+fi
+
+echo "$projectId" > BeamProjOverride.txt
+
+unrealPath="${UNREAL_ENGINE_PATH}"
+
+if [ -z "$UNREAL_ENGINE_PATH" ]; then
+    unrealPath="C:/Users/Administrator/Documents/Git/UnrealEngine/Engine"
+fi
+
+export UE_SELF_BUILT=1
+parentDirectory=$(dirname "$0")
+archiveDir="${parentDirectory}/PackagedProject"
+
+echo "Starting a $projectId $buildType build for $platform platform"
+
+"${unrealPath}/Binaries/ThirdParty/DotNet/6.0.302/windows/dotnet.exe" \
+    "${unrealPath}/Binaries/DotNET/UnrealBuildTool/UnrealBuildTool.dll" \
+    -ProjectFiles -UsePrecompiled -Game "${parentDirectory}/BeamableUnreal.uproject"
+"${unrealPath}/Build/BatchFiles/RunUAT.bat" BuildCookRun -project="${parentDirectory}/BeamableUnreal.uproject" \
+    -utf8output \
+    -platform="${platform}" \
+    "$extraArg" \
+    -noP4 -nodebuginfo -allmaps \
+    -cook -build -stage -prereqs -pak -archive \
+    -archivedirectory="${archiveDir}"
