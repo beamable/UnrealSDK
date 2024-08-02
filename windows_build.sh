3206e713ba0e80191a3f14868ed7d4e1a3bac9e0
Piotr Siuszko <siuszko@zoho.com>
2024-08-02 15:19:11 +0200

    Linux job fix, another build trigger

--- windows_build.sh
+++ windows_build.sh
@@ -34,6 +34,7 @@ else
     parentDirectory=$(realpath "$0")
 fi
 archiveDir="${parentDirectory}/PackagedProject"
+
 echo "Starting a $projectId $buildType build for $platform platform."
 
 rm -rf Plugins/Developer/RiderLink
