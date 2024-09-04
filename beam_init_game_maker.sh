9036b277554b33618b6f442778c5ba3a4ee27334
PedroRauizBeamable <92586258+PedroRauizBeamable@users.noreply.github.com>
2024-09-04 12:44:18 -0300

    Updated docs with clearer instructions on how to set up the Unreal SDK; Fixed issue on `init_repo.sh` script that could cause the CLI to not be installed properly; (#49)
    - Fixed issue that caused the Docs editing environment to require different paths than the ones that would work when they get published to GH-Pages.

--- beam_init_game_maker.sh
+++ beam_init_game_maker.sh
@@ -2,19 +2,25 @@
 
 if ! command -v dotnet >/dev/null 2>&1; then
     echo ".NET is not installed."
-    exit 1
+    return 1
 fi
 
 # Verify that the dotnet tool is callable by listing installed tools
 if ! dotnet tool list >/dev/null 2>&1; then
     echo "The dotnet tool cannot be called, which may indicate a misconfiguration. Make sure that Dotnet SDK is installed."
-    exit 1
+    return 1
 fi
 
 # Ensure we are inside a unreal project root folder.
-if [ ! -f "*.uproject" ]; then
+# Install the SDK in your project
+# Find the .uproject file path and name
+UPROJECT_PATH=$(find . -name "*.uproject" -print -quit)
+echo "Found .uproject: $UPROJECT_PATH"
+
+# Ensure we are inside a unreal project root folder.
+if [ $UPROJECT_PATH == "" ]; then
    echo "Please run this script from inside a folder containing a .uproject (your Unreal Project's root)."
-   exit 1
+   return 1
 fi
 
 # Check for .beamable directory
@@ -29,9 +35,9 @@ fi
 
 # Install Beamable.Tools with optional version
 if [ -z "$1" ]; then
-    dotnet tool install Beamable.Tools --version 2.0.0
+    dotnet tool install Beamable.Tools --version 2.0.1 --allow-downgrade
 else
-    dotnet tool install Beamable.Tools --version "$1"
+    dotnet tool install Beamable.Tools --version "$1" --allow-downgrade
 fi
 
-echo "Beamable Tools are installed. To call them use `dotnet beam` from any directory under your Unreal project root."
+echo "Beamable Tools are installed. To call them use \"dotnet beam\" from any directory under your Unreal project root."
