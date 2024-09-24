d8c9c666b67543a5f791826d60ad4a245f60486a
Aly Ameen <aly@1336studios.com>
2024-09-24 20:24:33 +0300

    LiveOps Demo Microservice

---
+++ Microservices/services/LiveOpsDemoMs/Program.cs
@@ -0,0 +1,20 @@
+﻿using Beamable.Server;
+using System.Threading.Tasks;
+
+namespace Beamable.LiveOpsDemoMs
+{
+	public class Program
+	{
+		/// <summary>
+		/// The entry point for the <see cref="LiveOpsDemoMs"/> service.
+		/// </summary>
+		public static async Task Main()
+		{
+			// inject data from the CLI.
+			await MicroserviceBootstrapper.Prepare<LiveOpsDemoMs>();
+			
+			// run the Microservice code
+			await MicroserviceBootstrapper.Start<LiveOpsDemoMs>();
+		}
+	}
+}
