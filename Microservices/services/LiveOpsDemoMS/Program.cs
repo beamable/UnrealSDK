467bb9c3d0fd0f66716d13dec6abf6d188d90d66
Aly Ameen <aly@1336studios.com>
2024-10-08 17:54:45 +0300

    Renaming the live ops Microservice name

---
+++ Microservices/services/LiveOpsDemoMS/Program.cs
@@ -0,0 +1,20 @@
+﻿using Beamable.Server;
+using System.Threading.Tasks;
+
+namespace Beamable.LiveOpsDemoMS
+{
+	public class Program
+	{
+		/// <summary>
+		/// The entry point for the <see cref="LiveOpsDemoMS"/> service.
+		/// </summary>
+		public static async Task Main()
+		{
+			// inject data from the CLI.
+			await MicroserviceBootstrapper.Prepare<LiveOpsDemoMS>();
+			
+			// run the Microservice code
+			await MicroserviceBootstrapper.Start<LiveOpsDemoMS>();
+		}
+	}
+}
