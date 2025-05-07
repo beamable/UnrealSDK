b6a09ea21d6a6cbc04a866543c7b32a54f4b2452
Vitor Balbio <vitor@beamable.com>
2025-05-07 14:10:33 -0300

    New Content Editor and Test Classes

---
+++ Plugins/BEAMPROJ_Beamball/Source/BEAMPROJ_Beamball/Public/SecondItemContent.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Content/BeamContentTypes/BeamItemContent.h"
+#include "SecondItemContent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BEAMPROJ_BEAMBALL_API USecondItemContent : public UBeamItemContent
+{
+	GENERATED_BODY()
+public:
+	// Define the ContentTypeId for this Type
+	UFUNCTION()
+	void GetContentType_USecondItemContent(FString& Result){Result = TEXT("secondcontent");};
+};
