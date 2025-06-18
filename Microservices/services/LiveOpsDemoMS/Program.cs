1f7cea0add1ffe689c24a29f50240a994051a00a
felipe-beamable <felipe@beamable.com>
2025-06-18 11:18:44 -0300

    Change - Re-Add the liveops sample

---
+++ Microservices/services/LiveOpsDemoMS/Program.cs
@@ -0,0 +1,24 @@
+﻿using Beamable.Server;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Beamable.LiveOpsDemoMS
+{
+	public class Program
+	{
+		/// <summary>
+		/// The entry point for the <see cref="HathoraDemo"/> service.
+		/// </summary>
+		public static async Task Main()
+		{
+			// inject data from the CLI
+			await MicroserviceBootstrapper.Prepare<LiveOpsDemoMS>();
+			
+			// run the Microservice code
+			await MicroserviceBootstrapper.Start<LiveOpsDemoMS>();
+		}
+	}
+}
