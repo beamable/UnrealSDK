4e5b47dab76e6e170bdfecb2649e9dee64ed74b7
felipe-beamable <felipe@beamable.com>
2025-08-18 16:00:51 -0300

    Change - Added a validation for PIE settings when entering in play mode.

---
+++ Plugins/BeamableCore/Source/BeamableCoreRuntimeEditor/Public/PIE/BeamPIEModularFeature.h
@@ -0,0 +1,85 @@
+#pragma once
+
+#include "BeamCoreSettings.h"
+#include "IPIEAuthorizer.h"
+#include "PIE/BeamPIEConfig.h"
+#include "PIE/BeamPIE_Settings.h"
+
+
+class UBeamPIEModularFeature: public IPIEAuthorizer
+{
+public:
+
+	virtual bool RequestPIEPermission(bool bIsSimulateInEditor, FString& OutReason) const override
+	{
+		auto BeamCoreSettings = GetDefault<UBeamCoreSettings>();
+		if (!BeamCoreSettings->bEnableBeamPIE)
+		{
+			return true;
+		}
+		
+		auto PlaySettings = GetMutableDefault<ULevelEditorPlaySettings>();
+		
+		int NumOfPlayers;
+		PlaySettings->GetPlayNumberOfClients(NumOfPlayers);
+
+		auto BeamPIEConfig = GetMutableDefault<UBeamPIEConfig>();
+		
+		FGuid SelectedSettingGUID;
+		SelectedSettingGUID.Invalidate();
+
+		auto Contexts = GEngine->GetWorldContexts();
+		for (auto Context : Contexts)
+		{
+			UE_LOG(LogTemp, Display, TEXT("GetWorldContexts %s"), *Context.World()->GetMapName());
+		}
+		if (Contexts.Num() == 0)
+		{
+			OutReason = TEXT("There's no level opened, you shouldn't be able to entering in the PIE mode.");
+			return false;
+		}
+		auto World = Contexts[0].World();
+		FString CurrentLevel = 	World->GetMapName();
+		if (BeamPIEConfig->PerMapSelection.Contains(CurrentLevel))
+		{
+			SelectedSettingGUID = BeamPIEConfig->PerMapSelection[CurrentLevel];
+		}
+		UE_LOG(LogTemp, Display, TEXT("Current Level: %s"), *CurrentLevel);
+		
+		if (!SelectedSettingGUID.IsValid())
+		{
+			// If the selected settings is not valid that means there's no select settings for this level
+			// So we just allow you to continue.
+			return true;
+		}
+		
+		FBeamPIE_Settings SelectedSetting;
+		
+		for (auto Setting : BeamPIEConfig->AllSettings)
+		{
+			if (Setting.SettingsId == SelectedSettingGUID)
+			{
+				SelectedSetting = Setting;
+				break;
+			}
+		}
+		FString ErrorReason = TEXT("");
+
+		// Checking if the number of assigned user is the same as the play mode setup
+		ErrorReason +=  NumOfPlayers != SelectedSetting.AssignedUsers.Num() ? FString::Printf(TEXT("\nThe number of players in the PIE Settings should be the same as in the Play config. [PIE Settings Players: %d] [Play Config Players: %d]"), SelectedSetting.AssignedUsers.Num(), NumOfPlayers) : TEXT("");
+
+		// Checking the gamer tag for each assigned user
+		for (auto AssignedUser : SelectedSetting.AssignedUsers)
+		{
+			ErrorReason += AssignedUser.Value.GamerTag.AsString.IsEmpty() ? FString::Printf(TEXT("\nThe [PIE Index: %d UserSlot %s] is not configured correctly, it's missing the gamer tag."), AssignedUser.Key.PIEIndex, *AssignedUser.Key.Slot.Name) : TEXT("");
+		}
+		
+		if ( !ErrorReason.IsEmpty() )
+		{
+			OutReason = FString::Printf(TEXT("PIE Setting Failed: %s"), *ErrorReason);
+			return false; // PIE will be blocked
+		}
+
+		return true; // Allow PIE
+	}
+};
